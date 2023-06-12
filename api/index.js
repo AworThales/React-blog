f720c5c9fb13ff89601eede4e7b26f4faf0daf05
AworThales <thals4jesus@gmail.com>
2023-06-12 10:57:35 -0700

    initializing file

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
 const express = require("express");
-
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const port = 7000;
+
+// app.use("/thallo", (req, res) => {
+//     console.log("This is thallo URL");
+// })
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+        useUnifiedTopology: true,
+})
+.then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+app.listen(port, () => {
+    console.log("Backend is connected");
+});
