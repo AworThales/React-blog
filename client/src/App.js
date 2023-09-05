5e5e4e6c96b92250d01554762ddc790ba1f81dcc
AworThales <thals4jesus@gmail.com>
2023-09-05 16:54:12 +0100

    update file

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Single from "./pages/single/Single";
-import Settings from "./pages/ssettings/Settings";
+import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
 import Post from "./components/post/Post";
 import { BrowserRouter as Router, Routes,
 Route,
 Link
  } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const user = false;
+  const {user} = useContext(Context);
   return (
     <Router>
       <Topbar />
@@ -23,7 +25,7 @@ function App() {
           <Route path="/register" element={user ? <Home /> : <Register />} />
           <Route path="/login" element={user ? <Home /> : <Login />} />
           <Route path="/write" element={user ? <Write /> : <Register />} />
-          <Route path="/settingd" element={user ? <Settings /> : <Register />} />
+          <Route path="/settings" element={user ? <Settings /> : <Register />} />
           <Route path="/post/:postId" element={<Single />} />
         </Route>
         </Routes>
